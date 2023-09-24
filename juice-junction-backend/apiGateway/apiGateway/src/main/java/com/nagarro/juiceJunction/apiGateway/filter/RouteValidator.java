43a98aa9e07015de9844085284f49a51cc9c6782
Palak3230 <palkueng3230@gmail.com>
2023-09-24 22:30:22 +0530

    Code of Juice Junction

---
+++ juice-junction-backend/apiGateway/apiGateway/src/main/java/com/nagarro/juiceJunction/apiGateway/filter/RouteValidator.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.nagarro.juiceJunction.apiGateway.filter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouteValidator {
+	public static final List<String> openApiEndpoints = Arrays.asList(
+            "/user/signup",
+            "/user/token",
+            "/eureka"
+    );
+	public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
